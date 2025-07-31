6c7fe78fe16699fafdf244ae09fb3fc54aeb5da4
Vitalic66 <red-dot1@web.de>
2025-07-31 18:36:30 +0200

    Create VTO2311R.py

---
+++ configs/VTO2311R.py
@@ -0,0 +1,50 @@
+from .config import *
+
+DAHUA_FILES = OrderedDict([
+	("hwid", {
+		"required": True,
+		"type": DAHUA_TYPE.Plain
+	}),
+	("Install", {
+		"required": True,
+		"type": DAHUA_TYPE.Plain
+	}),
+	("dhdtb.bin.img", {
+		"required": True,
+		"type": DAHUA_TYPE.Plain,
+		"size": 0x00100000
+	}),
+	("kernel.img", {
+		"required": True,
+		"type": DAHUA_TYPE.Plain,
+		"size": 0x00600000
+	}),	
+	("romfs-x.squashfs.img", {
+		"required": True,
+		"type": DAHUA_TYPE.uImage | DAHUA_TYPE.SquashFS,
+		"size": 0x01a80000
+	}),
+	("pd-x.squashfs.img", {
+		"required": True,
+		"type": DAHUA_TYPE.uImage | DAHUA_TYPE.SquashFS,
+		"size": 0x00100000
+	}),
+	("data-x.squashfs.img", {
+		"required": True,
+		"type": DAHUA_TYPE.uImage | DAHUA_TYPE.SquashFS,
+		"size": 0x00780000
+	}),
+	("web-x.squashfs.img", {
+		"required": True,
+		"type": DAHUA_TYPE.uImage | DAHUA_TYPE.SquashFS,
+		"size": 0x00800000
+	}),
+	("check.img", {
+		"required": True,
+		"type": DAHUA_TYPE.uImage | DAHUA_TYPE.Plain
+	}),
+	("sign.img", {
+		"required": True,
+		"type": DAHUA_TYPE.uImage | DAHUA_TYPE.Plain
+	})
+])
